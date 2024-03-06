4734e62345f44c6b4aa74721aa043cbb11d74d03
JDRiches <tothepowercore@gmail.com>
2024-03-06 19:50:44 +0000

    Better Cards

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,13 +3,21 @@ import React from 'react'
 
 function BookCard({title, image, author}) {
     return (
-      <div className='bg-slate-200 p-5 gap-12 w-52 h-80 rounded-md col-span-1 shadow-md  flex flex-col '>
-          <div className='h-1/2'>
-              <img src={image} className=' object-fill h-48 w-96 rounded-md m-auto'/>
+      <div className='bg bg-white w-80 p-5 gap-6 rounded-md col-span-1 shadow-md  flex flex-row '>
+          <div className='w-1/2'>
+              <img src={image} className='shadow-xl object-fill h-48 w-32 rounded-md'/>
           </div>
-          <div className='bg-slate-200 justify-items-start rounded-md h-1/4 my-auto'>
-              <h1 className='font-bold line-clamp-3'>{title}</h1>
-              <h2 className='align-text-bottom'>{author}</h2>
+          <div className='flex flex-col w-1/2'>
+            <div className='rounded-md  '>
+                <h1 className='font-bold line-clamp-2'>{title}</h1>
+                <h2 className='align-text-bottom truncate'>{author}</h2>
+            </div>
+            <div className='h-full flex flex-row mx-auto align-bottom items-end'>
+                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                    Read?
+                </button>
+            </div>
+
           </div>
       </div>
     )
--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -9,11 +9,11 @@ function BookGrid({bookData}) {
     }
 
   return (
-    <div className=' bg-slate-300 m-5 rounded-md grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12 place-items-center py-12'>
+    <div className=' bg-slate-100 m-5 p-4 rounded-md grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12 place-items-center '>
          {bookData.map((book) => {
         const imageLink = "imageLinks" in book.volumeInfo ? book.volumeInfo.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
         const author = "authors" in book.volumeInfo ? book.volumeInfo.authors[0] : "Author Not Found"
-        return <button onClick={clickFunc}><BookCard title={book.volumeInfo.title} image={imageLink} author={author}/></button>
+        return <BookCard title={book.volumeInfo.title} image={imageLink} author={author}/>
       })}
 
     </div>
