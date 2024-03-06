da58c7d475495793b8edc245d8b968210ed7a0fc
JDRiches <tothepowercore@gmail.com>
2024-03-06 00:42:39 +0000

    bookcards

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 function BookCard({title, image, author}) {
     return (
-      <div className='bg-slate-200 p-5 gap-10 w-52 h-80 rounded-md col-span-1 shadow-md  flex flex-col '>
+      <div className='bg-slate-200 p-5 gap-12 w-52 h-80 rounded-md col-span-1 shadow-md  flex flex-col '>
           <div className='h-1/2'>
               <img src={image} className=' object-fill h-48 w-96 rounded-md m-auto'/>
           </div>
-          <div className='bg-slate-200 rounded-md align-text-bottom h-1/4 m-auto'>
-              <h1 className='font-bold'>{title}</h1>
+          <div className='bg-slate-200 justify-items-start rounded-md h-1/4 my-auto'>
+              <h1 className='font-bold line-clamp-3'>{title}</h1>
               <h2 className='align-text-bottom'>{author}</h2>
           </div>
       </div>
--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import BookCard from './BookCard'
 
+
 function BookGrid({bookData}) {
+
+    const clickFunc = () => {
+        console.log("Clicked!!")
+    }
+
   return (
     <div className=' bg-slate-300 m-5 rounded-md grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-12 place-items-center py-12'>
          {bookData.map((book) => {
-        console.log(book)
         const imageLink = "imageLinks" in book.volumeInfo ? book.volumeInfo.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
         const author = "authors" in book.volumeInfo ? book.volumeInfo.authors[0] : "Author Not Found"
-        return <BookCard title={book.volumeInfo.title} image={imageLink} author={author}/>
+        return <button onClick={clickFunc}><BookCard title={book.volumeInfo.title} image={imageLink} author={author}/></button>
       })}
+
     </div>
   )
 }
